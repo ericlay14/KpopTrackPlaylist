21325db99aea574df98f59432f4f7d6c7ef36f99
Your Name <youremail@domain.com>
2022-03-01 17:38:23 -0600

    create 2 jsx files and add style sheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Feed from "./Feed.jsx";
 
 const App = () => (
   <div>
     <h1>KPOP CARD TRACKLIST</h1>
+    <Feed />
   </div>
 );
 
---
+++ src/Feed.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Feed = () => (
+  <div>
+    <h1>TESTING 123</h1>
+  </div>
+);
+
+export default Feed;
